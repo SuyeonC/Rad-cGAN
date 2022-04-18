b9b7e1632ea780fa6943d4387aa2016a6153233a
SuyeonC <81354268+SuyeonC@users.noreply.github.com>
2022-04-18 16:56:13 +0900

    Update generate_data.py

--- model/generate_data.py
+++ model/generate_data.py
@@ -6,7 +6,7 @@
 
 import numpy as np
 
-def generate_data(data):
+def generate_data(data,min_train,max_train):
     # data shape=(n_samples, row, col, timesteps)
     n_samples=data.shape[0]
     time_step=data.shape[3]
@@ -26,6 +26,10 @@ def generate_data(data):
             movie_in[i,::,::,j]=m_in
         m_out=(255.*((data[i,::,::,-1]+10.)/70.))+0.5
         movie_out[i,::,::,0]=m_out
+        
+    # Min-max scaling
+    movie_in=(movie_in-min_train)/(max_train-min_train)
+    movie_out=(movie_out-min_train)/(max_train-min_train)
 
     return movie_in, movie_out
 
